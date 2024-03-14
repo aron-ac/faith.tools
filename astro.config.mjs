50c4114fd8b246ce685e191374625ca4be5e062b
aron-ac <112349319+aron-ac@users.noreply.github.com>
2024-03-14 10:34:56 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
+import node from '@astrojs/node';
 //import netlify from "@astrojs/netlify";
 import sentry from "@sentry/astro";
 import keystatic from '@keystatic/astro';
@@ -30,6 +31,11 @@ if (SENTRY_DSN && SENTRY_AUTH_TOKEN && SENTRY_PROJECT) {
   }));
 }
 
+export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 // https://astro.build/config
 //export default defineConfig({
  // integrations,
